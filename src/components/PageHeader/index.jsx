b71200ff90c0c09fb0dd85d9e6ff5a289cf0af94
Ngoc158 <2051063606@e.tlu.edu.vn>
2022-04-01 11:42:35 +0700

    [Bento-project]-Headerplus

--- src/components/PageHeader/index.jsx
+++ src/components/PageHeader/index.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import {
   Button,
+  Col,
   Container,
   Form,
   FormControl,
@@ -8,6 +9,7 @@ import {
   Navbar,
   NavDropdown,
   Offcanvas,
+  Row,
 } from "react-bootstrap";
 
 import "./index.css";
@@ -17,6 +19,11 @@ import { useNavigate } from "react-router-dom";
 const PageHeader = () => {
   const [showHome, setShowHome] = useState(false);
   const [showPages, setShowPages] = useState(false);
+  const [showMenu, setShowMenu] = useState(false);
+  const [showGallery, setShowGallery] = useState(false);
+  const [showBlog, setShowBlog] = useState(false);
+  const [showContact, setShowContact] = useState(false);
+  const [showReservation, setShowReservation] = useState(false);
 
   const navigate = useNavigate();
 
@@ -68,8 +75,70 @@ const PageHeader = () => {
               <NavDropdown.Item>About-us</NavDropdown.Item>
               <NavDropdown.Item>Terms {"&"} privacy</NavDropdown.Item>
             </NavDropdown>
-          </div>
 
+            {/* Menu dropdown */}
+            <NavDropdown
+              title="Menu"
+              id="basic-nav-dropdown"
+              show={showMenu}
+              onMouseEnter={() => showDropdown(showMenu, setShowMenu)}
+              onMouseLeave={() => hideDropdown(setShowMenu)}
+              className="menu-link"
+            >
+              <NavDropdown.Item>Menu style 1</NavDropdown.Item>
+              <NavDropdown.Item>Menu style 2</NavDropdown.Item>
+              <NavDropdown.Item>Menu style 3</NavDropdown.Item>
+            </NavDropdown>
+
+            {/*Gallery dropdown*/}
+            <NavDropdown
+              title="Gallery"
+              id="basic-nav-dropdown"
+              show={showGallery}
+              onMouseEnter={() => showDropdown(showGallery, setShowGallery)}
+              onMouseLeave={() => hideDropdown(setShowGallery)}
+              className="menu-link"
+            >
+              <NavDropdown.Item>Portofolio</NavDropdown.Item>
+              <NavDropdown.Item>Portofolio-3</NavDropdown.Item>
+              <NavDropdown.Item>Portofolio-4</NavDropdown.Item>
+            </NavDropdown>
+
+            {/*Blog dropdown*/}
+            <NavDropdown
+              title="Blog"
+              id="basic-nav-dropdown"
+              show={showBlog}
+              onMouseEnter={() => showDropdown(showBlog, setShowBlog)}
+              onMouseLeave={() => hideDropdown(setShowBlog)}
+              className="menu-link"
+            >
+              <NavDropdown.Item>Blog</NavDropdown.Item>
+              <NavDropdown.Item>Blog-single</NavDropdown.Item>
+            </NavDropdown>
+
+            {/*Contact dropdown*/}
+            <NavDropdown
+              title="Contact"
+              id="basic-nav-dropdown"
+              show={showContact}
+              onMouseEnter={() => showDropdown(showContact, setShowContact)}
+              onMouseLeave={() => hideDropdown(setShowContact)}
+              className="menu-link"
+            ></NavDropdown>
+
+            {/*Reservation dropdown*/}
+            <NavDropdown
+              title="Reservation"
+              id="basic-nav-dropdown"
+              show={showReservation}
+              onMouseEnter={() =>
+                showDropdown(showReservation, setShowReservation)
+              }
+              onMouseLeave={() => hideDropdown(setShowReservation)}
+              className="menu-link"
+            ></NavDropdown>
+          </div>
           {/* Toggle */}
           <Navbar.Toggle aria-controls="offcanvasNavbar" />
 
@@ -78,36 +147,19 @@ const PageHeader = () => {
             aria-labelledby="offcanvasNavbarLabel"
             placement="end"
           >
-            <Offcanvas.Header closeButton>
+            <Offcanvas.Header closeButton className="OurName">
               <Offcanvas.Title id="offcanvasNavbarLabel">
-                Offcanvas
+                Our Menu
               </Offcanvas.Title>
             </Offcanvas.Header>
-            <Offcanvas.Body>
-              <Nav className="justify-content-end flex-grow-1 pe-3">
-                <Nav.Link href="#action1">Home</Nav.Link>
-                <Nav.Link href="#action2">Link</Nav.Link>
-                <NavDropdown title="Dropdown" id="offcanvasNavbarDropdown">
-                  <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-                  <NavDropdown.Item href="#action4">
-                    Another action
-                  </NavDropdown.Item>
-                  <NavDropdown.Divider />
-                  <NavDropdown.Item href="#action5">
-                    Something else here
-                  </NavDropdown.Item>
-                </NavDropdown>
-              </Nav>
-              <Form className="d-flex">
-                <FormControl
-                  type="search"
-                  placeholder="Search"
-                  className="me-2"
-                  aria-label="Search"
-                />
-                <Button variant="outline-success">Search</Button>
-              </Form>
-            </Offcanvas.Body>
+
+            <Container>
+              <Row>
+                <Col xs>First, buderedt unor</Col>
+                <Col xs={{ order: 12 }}>Second, but last</Col>
+                <Col xs={{ order: 1 }}>Third, but second</Col>
+              </Row>
+            </Container>
           </Navbar.Offcanvas>
         </Container>
       </Navbar>
